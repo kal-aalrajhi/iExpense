b0abcf770ca270709535dcea962ad24a73eead44
Kal <kal.aalrajhi@gmail.com>
2023-06-21 09:04:13 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dr Cpt Blackbeard on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
